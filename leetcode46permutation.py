8d1ece175300dfa0ff9c4bc5af23443786e59430
qkwu <496719312@qq.com>
2018-11-08 10:53:51 +0800

    leetcode46

---
+++ leetcode46permutation.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(nums, res, [])
+        return res
+
+    def dfs(self, nums, res, path):
+        if not nums:
+            res.append(path)
+        else:
+            for i in range(len(nums)):
+                self.dfs(nums[:i] + nums[i + 1:], res, path + [nums[i]])
+
+
+nums = [1, 2, 3]
+sl = Solution()
+print(sl.permute(nums))
